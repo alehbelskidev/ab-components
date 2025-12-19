5155de04964031d6b0eb4752af9fc3e7f0b2b0a4
Aleh Belski <aleh.belski@absoftware.dev>
2025-12-19 18:36:37 +0100

    fixing test runner and adding precommit hook

--- packages/components/vitest.config.ts
+++ packages/components/vitest.config.ts
@@ -1,4 +1,5 @@
 /// <reference types="vitest" />
+import path from "node:path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vitest/config"
 
@@ -13,6 +14,13 @@ export default defineConfig({
 		include: ["calendar/**/*.test.{ts,tsx}"],
 		coverage: {
 			reporter: ["text", "json", "html"],
+		},
+	},
+	resolve: {
+		alias: {
+			// This maps the "@" alias to the playground's src directory
+			// so Vite can find your utils, buttons, etc.
+			"@": path.resolve(__dirname, "../../playground/src"),
 		},
 	},
 })
